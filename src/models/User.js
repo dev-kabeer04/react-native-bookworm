d6ce27db5b21911d139d7de7d7bb518b52eef8bf
Kabeer <kabeer@Pixalive.me>
2025-07-14 14:32:06 +0100

    updated

--- src/models/User.js
+++ src/models/User.js
@@ -1,48 +1,45 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  profileImage: {
-    type: String,
-    default: "",
-  },
-}, {timestamps: true}
+  { timestamps: true }
 );
 
+// hash password before saving user to db
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    return next(); // Don't rehash if not modified
-  }
+  if (!this.isModified("password")) return next();
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
 });
 
-// compare password function
-
+// compare password func
 userSchema.methods.comparePassword = async function (userPassword) {
-    return await bcrypt.compare(userPassword, this.password)
-}
+  return await bcrypt.compare(userPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
